d5fe1137feb2b8961890e123e024ced25d349df0
Karen Mejia <karen.mejia@softtek.com>
2022-01-21 17:38:01 -0600

    AddCategory actualizado

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useState } from 'react/cjs/react.development';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+
 
 export const AddCategory = ({setCategories}) =>{
 
